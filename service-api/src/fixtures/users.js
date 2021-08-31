a9f5fa7031fae957fe732a9f60d0c667b8d876b9
Chris Hutchinson <chris_hutchinson@me.com>
2021-08-31 18:17:51 +0100

    cause the service api Pact tests to fail

--- service-api/src/fixtures/users.js
+++ service-api/src/fixtures/users.js
@@ -2,11 +2,11 @@ module.exports.users = [
   {
     id: 100,
     name: "First Last",
-    email: "first.last@example.com",
+    emailAddress: "first.last@example.com",
   },
   {
     id: 101,
     name: "A Name",
-    email: "a.name@example.com",
+    emailAddress: "a.name@example.com",
   },
 ];
